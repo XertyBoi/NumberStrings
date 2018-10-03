b52c3b08b53ab96a1f7b940cf6f0fb5fdcc7d0cb
Harrison Coulter <harrison@harrisons-mbp.home>
2018-10-03 19:54:18 +0100

    test 2 - 2nd single digit int test

--- number_string/converter.rb
+++ number_string/converter.rb
@@ -1,3 +1,10 @@
-def convert(n)
-  "one"
+def convert(num)
+  case num
+  when 1
+    "one"
+  when 5
+    "five"
+  else
+    ""
+  end
 end
--- spec/number_spec.rb
+++ spec/number_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "Convert ints to string number" do
   it "returns the string of a single int" do
     expect( convert(1) ).to eq "one"
   end
+
+  it "returns the string of a single int" do
+    expect( convert(5) ).to eq "five"
+  end
 end
