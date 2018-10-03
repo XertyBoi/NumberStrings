7d84dfac6c9cfa707890a86234214b4cc5b2ebde
Harrison Coulter <harrison@harrisons-mbp.home>
2018-10-03 22:18:05 +0100

    Refractor-2 created bounds class in bounds_object.rb to remove repeated code

---
+++ number_string/bounds_object.rb
@@ -0,0 +1,11 @@
+class Bounds
+
+  attr_accessor :min_bounds,:divide,:size_String
+
+  def initialize(min,div,string)
+    @min_bounds = min
+    @divide = div
+    @size_String = string
+  end
+
+end
