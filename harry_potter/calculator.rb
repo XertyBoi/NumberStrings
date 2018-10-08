b0f3bfb5a040e654776528604df87f002dbe69f9
Harrison Coulter <harrison@Harrisons-MacBook-Pro.local>
2018-10-08 08:56:30 +0100

    slight refractor

--- harry_potter/calculator.rb
+++ harry_potter/calculator.rb
@@ -1,19 +1,20 @@
 def calculate(books)
-  if books.size > 1
-    if books.size == 2
-      if (books[0] != books[1])
-      (books.size*8.00)-((books.size*8.00 / 100)*5)
-      else
-      books.size*8.00
-      end
+
+  case books.size
+  when 2
+    if (books[0] != books[1])
+    (books.size*8.00)-((books.size*8.00 / 100)*5)
     else
-      if (books[0] != books[1]) && (books[1] != books[2])
-      (books.size*8.00)-((books.size*8.00 / 100)*10)
-      else
-      books.size*8.00
-      end
+    books.size*8.00
+    end
+  when 3
+    if (books[0] != books[1]) && (books[1] != books[2])
+    (books.size*8.00)-((books.size*8.00 / 100)*10)
+    else
+    books.size*8.00
     end
   else
     8.00
   end
+
 end
