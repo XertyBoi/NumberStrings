11a7c139e623f91c8a9f8a0860f955686db3f0e1
Harrison Coulter <harrison@Harrisons-MacBook-Pro.local>
2018-10-08 08:47:12 +0100

    5% discount spec

--- harry_potter/calculator.rb
+++ harry_potter/calculator.rb
@@ -1,7 +1,11 @@
 def calculate(books)
   if books.size > 1
-    books.size*8
+    if (books[0] != books[1])
+    (books.size*8.00)-((books.size*8.00 / 100)*5)
+    else
+    books.size*8.00
+    end
   else
-    8
+    8.00
   end
 end
--- spec/harry_potter_spec.rb
+++ spec/harry_potter_spec.rb
@@ -3,11 +3,15 @@ require_relative "../harry_potter/calculator"
 RSpec.describe "Calculates the correct discounted price of books" do
 
   it "checks that one book costs 8 euro" do
-    expect(calculate([1])).to eq 8
+    expect(calculate([1])).to eq 8.00
   end
 
   it "checks that 2 books of the same kind equal 16 euro" do
-    expect(calculate([1,1])).to eq 16
+    expect(calculate([1,1])).to eq 16.00
+  end
+
+  it "checks that 2 books of a different kind have a 5% discount" do
+    expect(calculate([1,2])).to eq 15.20
   end
 
 end
